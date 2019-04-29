9f4042b074473362d4c475ffc9747af113bf8b4b
TabzKid <kidane.tabo@gmail.com>
2019-04-29 19:54:22 +0200

    test: Création des tests unitaires pour tester la persistance de l'entité Session

--- src/test/java/ch/hearc/odi/koulutus/services/PersistenceServiceTest.java
+++ src/test/java/ch/hearc/odi/koulutus/services/PersistenceServiceTest.java
@@ -3,6 +3,8 @@ package ch.hearc.odi.koulutus.services;
 import static org.junit.Assert.*;
 
 import ch.hearc.odi.koulutus.business.Pojo;
+import ch.hearc.odi.koulutus.business.Session;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -32,5 +34,47 @@ public class PersistenceServiceTest {
     Pojo actualPojo = entityManager.find(Pojo.class, expectedPojoID);
 
     assertEquals(pojo, actualPojo);
+  }
+
+  @Test
+  public void getAllSessionsTest() {
+    String  sessionRoomName1 = "room1";
+    String  sessionRoomName2 = "room2";
+    String  sessionRoomName3 = "room3";
+
+    persistenceService.createAndPersistSession(null, null, null, sessionRoomName1);
+    persistenceService.createAndPersistSession(null, null, null, sessionRoomName2);
+    persistenceService.createAndPersistSession(null, null, null, sessionRoomName3);
+
+    List<Session> sessions = persistenceService.getSessions();
+
+    assertEquals(sessionRoomName1, sessions.get(0).getRoom());
+    assertEquals(sessionRoomName2, sessions.get(1).getRoom());
+    assertEquals(sessionRoomName3, sessions.get(2).getRoom());
+  }
+
+  @Test
+  public void getSessionByIdTest() {
+    long id = 2l;
+    String sessionRoomName = "room2";
+
+    persistenceService.createAndPersistSession(null, null, null, "room1");
+    persistenceService.createAndPersistSession(null, null, null, sessionRoomName);
+
+    Session session = persistenceService.getSessionById(id);
+
+    assertEquals(sessionRoomName, session.getRoom());
+  }
+
+  @Test
+  public void createAndPersistSessionTest() {
+    Session session;
+    String sessionRoomName = "room1";
+    session = persistenceService.createAndPersistSession(null, null, null, sessionRoomName);
+
+    Long sessionId = session.getId();
+    Session actualSession = persistenceService.getSessionById(sessionId);
+
+    assertSame(actualSession.getRoom(), sessionRoomName);
   }
 }
