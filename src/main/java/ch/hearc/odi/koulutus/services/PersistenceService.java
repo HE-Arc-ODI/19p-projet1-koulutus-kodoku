f8a9b0788c0e982ab3beb3fda48f8bc6718c1469
TabzKid <kidane.tabo@gmail.com>
2019-04-29 20:50:13 +0200

    feat(core): Implémentation persistance pour l'entité Program

--- src/main/java/ch/hearc/odi/koulutus/services/PersistenceService.java
+++ src/main/java/ch/hearc/odi/koulutus/services/PersistenceService.java
@@ -7,6 +7,7 @@ package ch.hearc.odi.koulutus.services;
 
 import ch.hearc.odi.koulutus.business.Course;
 import ch.hearc.odi.koulutus.business.Pojo;
+import ch.hearc.odi.koulutus.business.Program;
 import ch.hearc.odi.koulutus.business.Quarter;
 import ch.hearc.odi.koulutus.business.Session;
 import ch.hearc.odi.koulutus.business.Status;
@@ -33,7 +34,7 @@ public class PersistenceService {
     super.finalize();
   }
 
-  public Pojo createAndPersistAPojo(String myProperty){
+  public Pojo createAndPersistAPojo(String myProperty) {
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
     Pojo pojo = new Pojo();
@@ -44,10 +45,11 @@ public class PersistenceService {
     return pojo;
   }
 
-  public List<Session> getSessions(){
+  public List<Session> getSessions() {
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
-    List<Session> sessions = entityManager.createQuery("from Session", Session.class).getResultList();
+    List<Session> sessions = entityManager.createQuery("from Session", Session.class)
+        .getResultList();
     entityManager.getTransaction().commit();
     entityManager.close();
     return sessions;
@@ -62,7 +64,8 @@ public class PersistenceService {
     return session;
   }
 
-  public Session createAndPersistSession(Date startDateTime, Date endDateTime, Double price, String room){
+  public Session createAndPersistSession(Date startDateTime, Date endDateTime, Double price,
+      String room) {
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
     Session session = new Session(startDateTime, endDateTime, price, room);
@@ -72,7 +75,7 @@ public class PersistenceService {
     return session;
   }
 
-  public List<Course> getCourses(){
+  public List<Course> getCourses() {
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
     List<Course> courses = entityManager.createQuery("from Course", Course.class).getResultList();
@@ -84,14 +87,14 @@ public class PersistenceService {
   public Course getCourseById(Long courseId) {
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
-    Course session = entityManager.find(Course.class, courseId);
+    Course course = entityManager.find(Course.class, courseId);
     entityManager.getTransaction().commit();
     entityManager.close();
-    return session;
+    return course;
   }
 
   public Course createAndPersistCourse(Quarter quarter, int year, int maxNumberOfParticipants,
-      Status status, List<Session> sessions){
+      Status status, List<Session> sessions) {
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
     Course course = new Course(quarter, year, maxNumberOfParticipants, status, sessions);
@@ -101,10 +104,36 @@ public class PersistenceService {
     return course;
   }
 
+  public List<Program> getPrograms() {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    List<Program> programs = entityManager.createQuery("from Program", Program.class)
+        .getResultList();
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return programs;
+  }
 
+  public Program getProgramById(Long programId) {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    Program program = entityManager.find(Program.class, programId);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return program;
+  }
 
-
-
+  public Program createAndPersistProgram(String name, String richDescription, String field,
+      int price,
+      List<Course> courses) {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    Program program = new Program(name, richDescription, field, price, courses);
+    entityManager.persist(program);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return program;
+  }
 
 }
 
