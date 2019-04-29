d95d3710d7f2e8aa7657a758aa519cd433130c62
TabzKid <kidane.tabo@gmail.com>
2019-04-29 19:58:41 +0200

    feat(core): Implémentation persistance pour l'entité Session

--- src/main/java/ch/hearc/odi/koulutus/services/PersistenceService.java
+++ src/main/java/ch/hearc/odi/koulutus/services/PersistenceService.java
@@ -6,6 +6,9 @@ package ch.hearc.odi.koulutus.services;
 
 
 import ch.hearc.odi.koulutus.business.Pojo;
+import ch.hearc.odi.koulutus.business.Session;
+import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -37,6 +40,38 @@ public class PersistenceService {
     entityManager.close();
     return pojo;
   }
+
+  public List<Session> getSessions(){
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    List<Session> sessions = entityManager.createQuery("from Session", Session.class).getResultList();
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return sessions;
+  }
+
+  public Session getSessionById(Long sessionId) {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    Session session = entityManager.find(Session.class, sessionId);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return session;
+  }
+
+  public Session createAndPersistSession(Date startDateTime, Date endDateTime, Double price, String room){
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    Session session = new Session(startDateTime, endDateTime, price, room);
+    entityManager.persist(session);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return session;
+  }
+
+
+
+
 
 }
 
