fd2b1a2f656c578e969d5e17a7b269c218599cb1
Ulysse Rosselet <urosselet@gmail.com>
2019-05-08 10:48:51 +0200

    FIX: ajouté le produces pour éviter les erreurs de marshalling

--- src/main/java/ch/hearc/odi/koulutus/rest/ProgramResourceRest.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ProgramResourceRest.java
@@ -10,8 +10,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Path("program")
+@Produces("application/json")
 public class ProgramResourceRest {
 
   @Inject
